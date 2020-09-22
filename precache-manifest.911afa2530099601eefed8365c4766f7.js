bf8297c5a6216fb9e0550f60e230ac4982a1ee7a
Shamika Kumar <shamika_kumar@Shamikas-MacBook-Pro.local>
2020-09-22 15:30:41 +0530

    Updates

---
+++ precache-manifest.911afa2530099601eefed8365c4766f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f9e2c5e523f4cbee71413ba71ea5ea",
+    "url": "/people-search/index.html"
+  },
+  {
+    "revision": "f499623926993b2272a4",
+    "url": "/people-search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "120fc293ce345cb59873",
+    "url": "/people-search/static/css/main.a75d99a8.chunk.css"
+  },
+  {
+    "revision": "f499623926993b2272a4",
+    "url": "/people-search/static/js/2.bae85daa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/people-search/static/js/2.bae85daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120fc293ce345cb59873",
+    "url": "/people-search/static/js/main.c06392ed.chunk.js"
+  },
+  {
+    "revision": "5786a67754d3377f4124",
+    "url": "/people-search/static/js/runtime-main.da0b897b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/people-search/static/media/SEGA.12a3db8b.woff"
+  }
+]);
